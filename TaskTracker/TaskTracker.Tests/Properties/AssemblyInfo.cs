f28f41f81db91c04cf943cad4a5bb8262c0c44bf
Dalmiro <dalmiro.granias@gmail.com>
2016-06-30 02:02:31 -0300

    Added test project and initial tests

--- TaskTracker/TaskTracker.Tests/Properties/AssemblyInfo.cs
+++ TaskTracker/TaskTracker.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TaskTracker.Tests")]
+[assembly: AssemblyTitle("TaskTracker.tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TaskTracker.Tests")]
+[assembly: AssemblyProduct("TaskTracker.tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("621f6500-50af-42fb-bdbb-b3282c84c768")]
+[assembly: Guid("798870ea-14e9-42f7-8d25-f2f5a86121bc")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ TaskTracker/TaskTracker.tests/TaskControllerFixture.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using TaskTracker.Controllers;
+
+namespace TaskTracker.Tests
+{
+
+    [TestFixture]
+    public class TaskControllerFixture
+    {
+        [Test]
+        public void Can_Get_All_Tasks()
+        {
+            var controller = new TaskController();
+            var allTasks = controller.Get().ToList();
+
+            Assert.That(allTasks, Has.Count.GreaterThan(0));
+        }
+
+        [Test]
+        public void Can_Get_Transit_Route_By_Id()
+        {
+            const int id = 1;
+
+            var controller = new TaskController();
+            var task = controller.Get(id);
+
+            Assert.That(task.Id, Is.EqualTo(id));
+
+        }
+    }
+
+}
