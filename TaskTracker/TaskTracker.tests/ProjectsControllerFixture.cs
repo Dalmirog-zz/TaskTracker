47b7c4c61d87f046b6c7c17ae7a14fe2133344d8
Dalmiro <dalmiro.granias@gmail.com>
2016-07-01 00:29:04 -0300

    Adding Project resource class, test data and tests

---
+++ TaskTracker/TaskTracker.tests/ProjectsControllerFixture.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Controllers;
+
+namespace TaskTracker.tests
+{
+    [TestFixture]
+    public class ProjectsControllerFixture
+    {
+        private ProjectsController PController = new ProjectsController();
+
+        [Test]
+        public void Can_Get_All_Projects()
+        {
+            var allProjects = PController.Get().ToList();
+            Assert.That(allProjects, Has.Count.GreaterThan(0));
+        }
+
+        [Test]
+        public void Can_Get_Project_By_Id()
+        {
+            var allProjects = PController.Get().ToList();
+            int i = new Random().Next(0,allProjects.Count - 1);
+            var project = allProjects[i];
+
+            Assert.That(PController.Get(project.Id).Name, Is.EqualTo(project.Name));
+        }
+    }
+}
---
+++ TaskTracker/TaskTracker/Controllers/ProjectsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TaskTracker.Models;
+
+namespace TaskTracker.Controllers
+{
+    public class ProjectsController : ApiController
+    {
+        private readonly List<Project> projects = new List<Project>();
+
+        public ProjectsController()
+        {
+            projects.Add(new Project { Id = 1, Name = "OctopusSupport", Description = "http://Tender.com/tickets/1" });
+            projects.Add(new Project { Id = 2, Name = "TaskTrackerDev", Description = "http://HelpScout.com/tickets/1" });
+            projects.Add(new Project { Id = 3, Name = "OctoposhDev", Description = "Reviewed backlog with #Team-Awesome" });
+        }
+
+        // GET: api/Projects
+        public IEnumerable<Project> Get()
+        {
+            return projects;
+        }
+
+        // GET: api/Projects/5
+        public Project Get(int id)
+        {
+            return projects.FirstOrDefault(p => p.Id == id);
+        }
+
+        // POST: api/Projects
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Projects/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Projects/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
