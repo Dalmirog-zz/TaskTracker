5e131d7a00edbd73a8a1f4ed9eb5f4cff0bc2875
Dalmiro <dalmiro.granias@gmail.com>
2016-07-04 02:50:57 -0300

    Adding Project Repository tests

---
+++ TaskTracker/TaskTracker.tests/ProjectsRepositoryFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TaskTracker.Controllers;
+using TaskTracker.Models;
+
+namespace TaskTracker.tests
+{
+    [TestFixture]
+    public class ProjectsRepositoryFixture
+    {
+        private Repository Repository = new Repository();
+
+        [Test]
+        public void Can_Get_All_Projects_From_Repository()
+        {
+            var allProjects = Repository.Projects.GetAll();
+            Assert.That(allProjects, Has.Count.GreaterThan(0));
+        }
+    }
+}
