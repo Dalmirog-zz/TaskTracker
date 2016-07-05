9bcd1e4b4d3e5c93bc8d6e09d762cf51e8ad6d84
Dalmiro <dalmiro.granias@gmail.com>
2016-07-04 23:47:45 -0300

    re-adding test project

---
+++ TaskTracker/TaskTracker.Tests/Controllers/ProjectsControllerFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TaskTracker.Controllers;
+
+namespace TaskTracker.Tests.Controllers
+{
+    [TestFixture]
+    public class ProjectsControllerFixture
+    {
+        private ProjectsController PController = new ProjectsController();
+
+        [Test]
+        public void Can_Get_All_Projects_From_Controller()
+        {
+            var allProjects = PController.Get().ToList();
+            Assert.That(allProjects, Has.Count.GreaterThan(0));
+        }
+
+        [Test]
+        public void Can_Get_Project_By_Id_From_Controller()
+        {
+            var allProjects = PController.Get().ToList();
+            int i = new Random().Next(0,allProjects.Count - 1);
+            var project = allProjects[i];
+
+            Assert.That(PController.Get(project.Id).Name, Is.EqualTo(project.Name));
+        }
+    }
+}
