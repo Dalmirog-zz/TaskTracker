f2daa4c366146b0e5a087f7dc8bd4bfac0545c60
Dalmiro <dalmiro.granias@gmail.com>
2016-07-09 20:42:06 -0300

    Adding Project and Tag to task

--- TaskTracker/TaskTracker/Models/Task.cs
+++ TaskTracker/TaskTracker/Models/Task.cs
@@ -10,5 +10,7 @@ namespace TaskTracker.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Project Project { get; set; }
+        public List<Tag> Tags { get; set; }
     }
 }
