e20daef23e353835768959ae7447bf19d36168d2
Dalmiro <dalmiro.granias@gmail.com>
2016-08-21 11:46:05 -0300

    End of module "Startup and Middleware"

--- dotNetcore/TaskTracker/src/TaskTracker/Startup.cs
+++ dotNetcore/TaskTracker/src/TaskTracker/Startup.cs
@@ -22,6 +22,7 @@ namespace TaskTracker
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddSingleton(provider => Configuration);
             services.AddSingleton<IGreeter, Greeter>();
         }
@@ -29,9 +30,21 @@ namespace TaskTracker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(
             IApplicationBuilder app,
+            IHostingEnvironment environment,
             IGreeter greeter)
         {
             app.UseIISPlatformHandler();
+
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRuntimeInfoPage("/info");
+
+            app.UseFileServer();
+
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
