52a4df640d354a891db8155b81111e6303a8b216
Dalmiro <dalmiro.granias@gmail.com>
2016-08-20 23:26:13 -0300

    End of module "Building your first ASP.NET Core application"
    https://app.pluralsight.com/player?course=aspdotnet-core-1-0-fundamentals&author=scott-allen&name=aspdotnet-core-1-0-fundamentals-m1&clip=6&mode=live

--- dotNetcore/TaskTracker/src/TaskTracker/Startup.cs
+++ dotNetcore/TaskTracker/src/TaskTracker/Startup.cs
@@ -1,30 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TaskTracker.Services;
 
 namespace TaskTracker
 {
     public class Startup
     {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(
+            IApplicationBuilder app,
+            IGreeter greeter)
         {
             app.UseIISPlatformHandler();
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
             });
         }
 
