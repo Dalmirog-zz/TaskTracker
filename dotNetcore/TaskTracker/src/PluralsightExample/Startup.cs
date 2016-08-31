c42831ede74af11bf02b196674747ee905046543
Dalmiro <dalmiro.granias@gmail.com>
2016-08-31 02:19:09 -0300

    adding .netcore tasktracker

---
+++ dotNetcore/TaskTracker/src/PluralsightExample/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskTracker.Services;
+
+namespace TaskTracker
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton(provider => Configuration); 
+            services.AddSingleton<IGreeter, Greeter>(); //SINGLETON: Only one instance of the object will be created and It'll be shared across HTTP requests.
+            services.AddScoped<IRestaurantData, InMemotyRestaurantData>(); //Each HTTP request will have its own object instance.
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment environment,
+            IGreeter greeter)
+        {
+            app.UseIISPlatformHandler();
+
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRuntimeInfoPage("/info");
+
+            app.UseFileServer();
+
+            app.UseMvc(ConfigureRoute);
+
+            app.Run(async (context) =>
+            {
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+
+        private void ConfigureRoute(IRouteBuilder routBuilder)
+        {
+            routBuilder.MapRoute("Default",
+                "{controller=Home}/{action=Index}/{id?}");
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
